a6b6eaf71d39379e3ae2e6a18d22c522aa17cb0d
Mezbaul Islam Protick <mezbaulislamprotick@outlook.com>
2020-10-03 00:49:50 +0600

    When I created the repo I didn't know about the git lfs. Due to lack of my knowledge I reinitialized my repo and lost my previous commits. I'm really sorry.

--- model_asset.py
+++ model_asset.py
@@ -1,12 +1,13 @@
 import wget
 
-url = ['https://colorizers.s3.us-east-2.amazonaws.com/colorization_release_v2-9b330a0b.pth', 
-    'https://colorizers.s3.us-east-2.amazonaws.com/siggraph17-df00044c.pth'
-]
+url = ['https://colorizers.s3.us-east-2.amazonaws.com/colorization_release_v2-9b330a0b.pth',
+       'https://colorizers.s3.us-east-2.amazonaws.com/siggraph17-df00044c.pth'
+    ]
 
-dir = ['models/colorization_release_v2-9b330a0b.pth', 
-'models/siggraph17-df00044c.pth'
-]
+dir = ['models/colorization_release_v2-9b330a0b.pth',
+       'models/siggraph17-df00044c.pth'
+    ]
 
 for i in range(2):
     wget.download(url[i], dir[i])
+
